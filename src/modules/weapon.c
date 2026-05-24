de4ca24a8c1475b31c05388d3824237077ff2ffe
SynaMax <11140222+synamaxmusic@users.noreply.github.com>
2026-05-24 01:10:39 +0000

    Type fix

--- src/modules/weapon.c
+++ src/modules/weapon.c
@@ -4,9 +4,9 @@
 #include "weapon.h"
 #include "global_exports.h"
 
-extern UnkStruct_weapon_00400184 D_weapon_00400184;
+extern void* D_weapon_00400184;
 extern s32 D_weapon_00400198;
-extern s32 func_weapon_004000E0;
+//extern s32 func_weapon_004000E0;
 
 void __entrypoint_func_weapon_400000(Weapon_Exports *exports) {
     s32 i;
